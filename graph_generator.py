213cb0cf92b5a4fedf23f74d074b46c2d542274c
varunakantha <varunakantha@gmail.com>
2022-12-01 17:07:10 +0530

    Bug fix : Removed all thread join() so that graph-thread get restarted smoothly.

--- graph_generator.py
+++ graph_generator.py
@@ -19,33 +19,34 @@ def plotdata():
             data = pd.read_excel('Data\graph.xlsx')
             lock.release()
 
+            if len(data) > 0:
+                x = data['time'].to_list()
+                x1 = data['ftime'].to_list()
+                y1 = data['count'].to_list()
+                z = data['predicted_value'].to_list()
+
+                y_counter = data['time'].iat[-1]
+                current_a = data['a'].iat[-1]
+                current_b = data['b'].iat[-1]
+                current_c = data['c'].iat[-1]
+
+                plt.cla()
+                plt.scatter(x, y1, color='black')
+                plt.plot(x, y1, label='Channel 1')
+                plt.plot(x1, z, label='Channel 2')
+
+                # plt.legend(loc='upper left')
+                title = "y = (" + str(current_a) + ")x^2 + (" + str(current_b) + ")x + " + str(
+                    current_c) + "    |   " + str(datetime.datetime.now())
+                plt.title(title)
+                plt.tight_layout()
+                plt.savefig("Data/graph-" + str(y_counter) + ".png")
+
         except Exception as e:
             print("Oops ! Error in Animation-Thread : " + str(e))
+            plt.cla()
             t2 = threading.Thread(target=plotdata)
             t2.start()
-            t2.join()
-
-        if len(data) > 0:
-            x = data['time'].to_list()
-            x1 = data['ftime'].to_list()
-            y1 = data['count'].to_list()
-            z = data['predicted_value'].to_list()
-
-            y_counter = data['time'].iat[-1]
-            current_a = data['a'].iat[-1]
-            current_b = data['b'].iat[-1]
-            current_c = data['c'].iat[-1]
-
-            plt.cla()
-            plt.scatter(x, y1, color='black')
-            plt.plot(x, y1, label='Channel 1')
-            plt.plot(x1, z, label='Channel 2')
-
-            # plt.legend(loc='upper left')
-            title = "y = (" + str(current_a) + ")x^2 + (" + str(current_b) + ")x + " + str(current_c)+"    |   "+str(datetime.datetime.now())
-            plt.title(title)
-            plt.tight_layout()
-            plt.savefig("Data/graph-" + str(y_counter) + ".png")
 
     ani = FuncAnimation(plt.gcf(), animate, interval=10000)
 
--- main.py
+++ main.py
@@ -10,33 +10,22 @@ import pandas as pd
 
 import executors
 
-
-target_page = "https://gossip.hirufm.lk/81445/2022/11/fifa-world-cup-qatar-2022.html"
+target_page = "https://gossip.hirufm.lk/81509/2022/11/hashan-pannila-photos.html"
 target_element = "/html/body/div[1]/div[6]/div[1]/div[2]/div/div[3]/div[3]/div/div[1]/font[1]/b"
+
 
 def exe_func():
     executor = executors.MainExecutor()
 
-    #if executor.login("varunakantha@gmail.com", "xxx"):
+    # if executor.login("varunakantha@gmail.com", "xxx"):
 
-    try:
-        executor.start_data_stream(target_page, target_element)
-    
-    except Exception as e:
-            print("Oops! Exception in main method : " + str(e))
-            time.sleep(5)
-            executor.start_data_stream(target_page, target_element)
-
+    executor.start_data_stream(target_page, target_element)
 
 
 if __name__ == '__main__':
-    pd.DataFrame(columns=['time','count','a','b','c']).to_excel('Data\graph.xlsx')
-    t1 = threading.Thread(target= exe_func)
-    t2 = threading.Thread(target = plotdata)
-
+    pd.DataFrame(columns=['time', 'count', 'a', 'b', 'c']).to_excel('Data\graph.xlsx')
+    t1 = threading.Thread(target=exe_func)
+    t2 = threading.Thread(target=plotdata, daemon=True)
     t1.start()
     t2.start()
-    
-    t1.join()
-    t2.join()
 
